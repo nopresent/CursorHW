548e84100f810da838ba034e6b4bc04a3051c57f
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-10 18:17:09 +0300

    Вышло не плохо )

--- src/com/cursorhw/hw8/Visitor.java
+++ src/com/cursorhw/hw8/Visitor.java
@@ -11,29 +11,53 @@ public class Visitor extends LibraryVisitors {
         DayMap.put("y", localDate.getYear());
         DayMap.put("m", localDate.getMonthValue());
         DayMap.put("d", localDate.getDayOfMonth());
-        getReport().put(DayMap, titleOfTheBook);
+
+        // Если в указанную дату не было книг, создаем новый мааси книг и добавляем туда книгу N1
+        ArrayList<String> bookArray = new ArrayList<>();
+
+        if (searchTitleByDate(localDate).get(0) != getNoBook()) {
+            // Такой день уже есть, находим его и дописываем книгу
+            TreeMap report = getReport();
+
+            Set set = report.entrySet();
+            Iterator iterator = set.iterator();
+
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator.next();  // {d=25, y=2021, m=9}=Book 1
+                bookArray = (ArrayList<String>) entry.getValue();
+            }
+        }
+
+        bookArray.add(titleOfTheBook);
+        getReport().put(DayMap, bookArray);
     }
+
 }
 
 abstract class LibraryVisitors implements LibraryReport {
 
     private String noBook = "There are no books for this date";
+
+    public String getNoBook() {
+        return noBook;
+    }
+
     /*
-     report - для хранения дат и книг
-     20211001 - DayMap
-       Книга 1 - ArrayList
-       Книга 2
-     20211002
-       Книга 1
-   */
-    private TreeMap<HashMap<String, Integer>, String> report = new TreeMap<>(new Comparator<HashMap<String, Integer>>() {
+         report - для хранения дат и книг
+         20211001 - DayMap
+           Книга 1 - ArrayList
+           Книга 2
+         20211002
+           Книга 1
+       */
+    private TreeMap<HashMap<String, Integer>, ArrayList<String>> report = new TreeMap<>(new Comparator<HashMap<String, Integer>>() {
         @Override
         public int compare(HashMap<String, Integer> o1, HashMap<String, Integer> o2) {
             return hashToLocalDate(o1).compareTo(hashToLocalDate(o2));
         }
     });
 
-    public TreeMap<HashMap<String, Integer>, String> getReport() {
+    public TreeMap<HashMap<String, Integer>, ArrayList<String>> getReport() {
         return report;
     }
 
@@ -42,19 +66,19 @@ abstract class LibraryVisitors implements LibraryReport {
     }
 
     // Поиск книги по дате LocalDate
-    public String searchTitleByDate(LocalDate localDate) {
+    public ArrayList searchTitleByDate(LocalDate localDate) {
         Set set = report.entrySet();
         Iterator iterator = set.iterator();
-        String answer = noBook;
+        ArrayList answer = new ArrayList();
+        answer.add(noBook);
 
         while (iterator.hasNext()) {
             Map.Entry entry = (Map.Entry) iterator.next();  // {d=25, y=2021, m=9}=Book 1
             HashMap<String, Integer> entryHm = (HashMap<String, Integer>) entry.getKey();
             if (localDate.equals(hashToLocalDate(entryHm))) {
-                answer = (String) entry.getValue();
+                answer = (ArrayList) entry.getValue();
             }
         }
-
         return answer;
     }
 
@@ -81,13 +105,28 @@ abstract class LibraryVisitors implements LibraryReport {
         }
 
         while (!dStart.equals(dEnd.plusDays(1))) {
-            String bookTitle = searchTitleByDate(dStart);
-            if (searchTitleByDate(dStart) == noBook) {
-                bookTitle = "0";
+            ArrayList<String> arrayBookTitle = searchTitleByDate(dStart);
+            if (arrayBookTitle.get(0) == noBook) {
+                System.out.println(dStart + " - 0");
+            } else {
+                System.out.println(dStart + " - " + arrayBookTitle.size());
             }
-            System.out.println(dStart + " - " + bookTitle);
             dStart = dStart.plusDays(1);
         }
+    }
+
+    public void getListOfTheBook() {
+        Set set = report.entrySet();
+        Iterator iterator = set.iterator();
+        ArrayList<String> arrayListOut = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry) iterator.next();  // {d=25, y=2021, m=9}=Book 1
+            ArrayList<String> arrayListIn = (ArrayList<String>) entry.getValue();
+            arrayListOut.addAll(arrayListIn);
+        }
+
+        System.out.println(arrayListOut);
     }
 
 }
