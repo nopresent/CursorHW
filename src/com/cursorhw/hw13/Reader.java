ef5e3b7680f1d2e54910a2abd74d63e27f460dd9
Dmitrij-Lenovo <nopresent@gmail.com>
2021-10-29 01:56:54 +0300

    commit

--- src/com/cursorhw/hw13/Reader.java
+++ src/com/cursorhw/hw13/Reader.java
@@ -1,17 +1,22 @@
 package com.cursorhw.hw13;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Reader {
-     public static void main(String[] args) throws IOException {
+     public static void main(String[] args) {
 
-        PrintWriter fw = new PrintWriter("file.txt");
+         File file = new File("file.txt");
 
-        fw.println("Привет!");
-
-        fw.flush();
-        fw.close();
-
+        try (PrintWriter fw = new PrintWriter("file.txt");
+             FileReader fr = new FileReader("file.txt")) {
+            fw.println("Привет!");
+            fw.println("Привет!");
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода/вывода данных");
+        }
     }
 }
